41a640bf25e4e07073548b3ab8c0b3fde5961af6
bruce manson <bruce.manson@outlook.com>
2022-11-26 01:32:05 +1300

    test

--- main.lua
+++ main.lua
@@ -1,21 +1,65 @@
---v1.0.0
-local frame = CreateFrame("Frame")
-local events = {}
+local Frame = CreateFrame("Frame")
+Frame:RegisterEvent("PLAYER_LOGIN")
 
-function events:NAME_PLATE_UNIT_ADDED(plate)
-	local nameplate = C_NamePlate.GetNamePlateForUnit(plate)
-	local frame = nameplate.UnitFrame
-	if not nameplate or frame:IsForbidden() then return end
-	local text_name = frame.healthBar.unitName:GetText()
-	print("Hello: ",text_name)
-	-- if (text) then
-    --     local  a ,b, c ,d ,e ,f =  strsplit(" ",text,5)   
-	-- 	unitFrame.healthBar.unitName:SetText (f or e or d or c or b or a)  
-    -- end 	
+Frame:SetScript("OnEvent", function(...)
+	SetCVar("nameplateMaxDistance", 45)
+	SetCVar("nameplateMaxScale", 1.0)
+	SetCVar("nameplateMinScale", 1.0)
+	SetCVar("nameplateLargerScale", 1.15)
+	SetCVar("nameplateGlobalScale", 1.15)
+	SetCVar("nameplateSelectedScale", 1.15)
+
+	SetCVar("nameplateLargeBottomInset", -5) 
+	SetCVar("nameplateLargeTopInset", -5) 
+	SetCVar("nameplateOtherTopInset", -5)
+	SetCVar("nameplateOtherBottomInset", -5)
+
+	SetCVar('UnitNameGuildTitle', 0)
+	SetCVar('UnitNamePlayerPVPTitle', 0)
+	SetCVar("nameplateOverlapV", 0.6)
+	SetCVar("nameplateOccludedAlphaMult", 0.4)
+end)
+
+--  Nametag font size
+local function SetFont(obj,optSize)
+	local fontName=obj:GetFont();
+	obj:SetFont(fontName,optSize,"THINOUTLINE");
 end
 
-for e, u in pairs(events) do
-	frame:RegisterEvent(e)
-end
+SetFont(SystemFont_LargeNamePlate, 8.6)
+SetFont(SystemFont_NamePlate, 8.6)
+SetFont(SystemFont_LargeNamePlateFixed, 8.6)
+SetFont(SystemFont_NamePlateFixed, 8.6)
 
-frame:SetScript("OnEvent", function(self, event, ...) events[event](self, ...) end)
+
+--  Move nametag
+hooksecurefunc("DefaultCompactNamePlateFrameAnchorInternal",function(frame)
+	frame.name:ClearAllPoints();--  Clear nametag anchors
+	PixelUtil.SetPoint(frame.name,"BOTTOM",frame.healthBar,"TOP",0,2);--    Set new anchor
+end);
+
+--  Remove realm names
+hooksecurefunc("CompactUnitFrame_UpdateName",function(frame)
+	if ShouldShowName(frame) then
+		frame.name:SetVertexColor(1,1,1);-- Fixes tapped mobs permanently setting the nametag gray
+		frame.name:SetText(GetUnitName(frame.unit));
+	end
+end);
+
+
+-- move nameplates debuffs
+hooksecurefunc(NameplateBuffContainerMixin,"UpdateAnchor",function(self)
+    local parent=self:GetParent();
+    local unit=parent.unit;
+
+    if unit and ShouldShowName(parent) then
+--      Replicate the calculation of the original function
+        local offset=self:GetBaseYOffset()+((unit and UnitIsUnit(unit,"target")) and self:GetTargetYOffset() or 0);
+        self:SetPoint("BOTTOM",parent,"TOP",0,-20);--    Apply offset here
+    end--   We'll leave the false side of this alone to preserve the original anchor in that case
+end);
+
+-- nameplate buff size
+hooksecurefunc(NameplateBuffContainerMixin,"OnLoad",function(self)
+    self:SetScale(0.85);--   1 is default size.
+end);
