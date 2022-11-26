a75e9b39fb50c2be1ddcb750f4fb944ca31e98f1
bruce manson <bruce.manson@outlook.com>
2022-11-26 22:08:04 +1300

    BUG FIX

--- main.lua
+++ main.lua
@@ -87,22 +87,26 @@ end)
 
 -- Threat display.
 hooksecurefunc("CompactUnitFrame_UpdateHealthBorder", function(frame)
-	if not frame:IsForbidden() then
+	if frame.optionTable.colorNameBySelection and not frame:IsForbidden() then
 		-- Colorize unit frame depending on threat.
 		local threat = UnitThreatSituation("player", frame.unit) -- Gets unit threat level, 3 = tanking, 2 = insecurely tanking/losing threat, 1/0 = not tanking.
-
 		if C_NamePlate.GetNamePlateForUnit(frame.unit) ~= C_NamePlate.GetNamePlateForUnit("player") and frame.level then
 			if threat == 3 then --3 = Securely tanking; make borders red.
-				frame.healthBar.border:SetVertexColor(1, 0, 0, 1)
+				frame.W.border:SetVertexColor(1, 0, 0, 1)
+				print("Option A")
 			elseif threat == 2 then -- 2 = Tanking, but somebody else has higher threat (losing threat); make borders orange.
 				frame.healthBar.border:SetVertexColor(1, 0.5, 0, 1)
+				print("Option B")
 			elseif threat == 1 then -- 1 = Not tanking, but higher threat than tank; make borders yellow.
 				frame.healthBar.border:SetVertexColor(1, 1, 0.4, 1)
-						else -- 0 = Not tanking; make borders black.
+				print("Option C")
+			else -- 0 = Not tanking; make borders black.
+				print("Option D")
 				frame.healthBar.border:SetVertexColor(0, 0, 0, 1)
 			end
 		else
-			frame.healthBar.border:SetVertexColor(frame.optionTable.defaultBorderColor:GetRGBA()) -- Thanks Blizzard...
+			-- frame.healthBar.border:SetVertexColor(frame.optionTable.defaultBorderColor:GetRGBA()) -- Thanks Blizzard...
+			print("Option E")
 		end
 	end
 end)
